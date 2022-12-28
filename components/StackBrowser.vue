2db4eee21dcb7a00e5c4b4a0c4b14bf7ee5f2cf7
kiotosi <shilo@firecode.ru>
2022-12-28 16:26:34 +0300

    feat(components): add components for stack page

---
+++ components/StackBrowser.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="browser">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ pages/stack.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="stack">
+    <StackBrowser />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default-without-container',
+});
+</script>
+
+<style scoped lang="scss">
+</style>
